e33f6248fa3a1e893b40d7981f76eb5a50993bab
anooja62 <anooja7777@gmail.com>
2025-09-30 22:35:15 +0530

    feat(api): add root (/) route for basic  check

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes
+app.get("/", (req, res) => {
+  res.send("API is running ");
+});
 app.use("/api/auth", authRoutes);
 
 export default app;
